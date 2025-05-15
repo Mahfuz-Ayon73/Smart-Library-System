72b51d5a5c0120b565e3068d41a701f43d840cb3
Mahfuz Ayon <134989125+Mahfuz-Ayon73@users.noreply.github.com>
2025-05-15 12:37:16 +0600

    Add Controller
    Controller : Stats
    method : getAllActiveusers

--- Phase-1/Smart_Library_System/backend/controllers/stats.controller.js
+++ Phase-1/Smart_Library_System/backend/controllers/stats.controller.js
@@ -1,5 +1,6 @@
 import { getBook } from "./book.controller.js";
 import { getAllLoans } from "./loan.controllers.js";
+import { getUser } from "./user.controller.js";
 
 async function getPopularBooks(req, res) {
     try {
@@ -51,4 +52,56 @@ async function getPopularBooks(req, res) {
     }
 }
 
-export default getPopularBooks;
+async function getAllActiveUsers(req,res) {
+    try {
+        const allLoans = await getAllLoans();
+    
+        const userStats = new Map();
+    
+        for (const loan of allLoans) {
+          const userId = loan.user_id.toString();
+    
+          if (!userStats.has(userId)) {
+            userStats.set(userId, {
+              books_borrowed: 0,
+              current_borrows: 0,
+            });
+          }
+    
+          const stats = userStats.get(userId);
+          stats.books_borrowed += 1;
+    
+          if (loan.status === "ACTIVE") {
+            stats.current_borrows += 1;
+          }
+        }
+    
+        const sortedUsers = [...userStats.entries()]
+          .sort((a, b) => b[1].books_borrowed - a[1].books_borrowed)
+          .slice(0, 5); //top 5 user
+    
+        const activeUsers = await Promise.all(
+          sortedUsers.map(async ([userId, stats]) => {
+            const user = await getUser(userId);
+    
+            if (!user) return null;
+    
+            return {
+              user_id: user._id,
+              name: user.name,
+              books_borrowed: stats.books_borrowed,
+              current_borrows: stats.current_borrows,
+            };
+          })
+        );
+    
+        const response = activeUsers.filter((user) => user !== null);
+    
+        res.json(response);
+      } catch (error) {
+        console.error("Error in getActiveUsers:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+}
+
+export {getPopularBooks , getAllActiveUsers};
--- Phase-1/Smart_Library_System/backend/routes/stats.route.js
+++ Phase-1/Smart_Library_System/backend/routes/stats.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import getPopularBooks from "../controllers/stats.controller.js";
+import {getPopularBooks , getAllActiveUsers} from "../controllers/stats.controller.js";
 
 const router = express.Router();
 
 router.get('/books/popular',getPopularBooks);
+router.get('/users/active',getAllActiveUsers);
 
 export default router;
